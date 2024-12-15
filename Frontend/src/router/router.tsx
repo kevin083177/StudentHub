43dda3d69f9ac337226483bfc37b9f68e586b53c
kevin083177 <kevin083177@gmail.com>
2024-12-15 17:29:23 +0800

    add route: add, delete, update

--- Frontend/src/router/router.tsx
+++ Frontend/src/router/router.tsx
@@ -1,9 +1,23 @@
 import { createHashRouter } from "react-router";
 import App from '../view/App';
-
+import Delete from '../view/Delete';
+import UpdateName from "../view/Update";
+import AddUser from "../view/Add";
 export const router = createHashRouter([
     {
         path: "/",
         element: <App />,
+    },
+    {
+        path: "/add",
+        element: <AddUser />
+    },
+    {
+        path: "/delete",
+        element: <Delete />
+    },
+    {
+        path: "/update",
+        element: <UpdateName />
     },
 ])
