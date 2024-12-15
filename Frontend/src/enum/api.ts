cf6621f9db35cc2f769ef14aacc54134dd667917
kevin083177 <kevin083177@gmail.com>
2024-12-15 18:53:25 +0800

    add environment variables for api configuration in .env file

--- Frontend/src/enum/api.ts
+++ Frontend/src/enum/api.ts
@@ -1,3 +1,13 @@
-export enum api{
-    findAll = "http://163.13.201.151.:8877/api/v1/user/findAll"
-}
+const ip = import.meta.env.VITE_IP;
+const port = import.meta.env.VITE_PORT;
+
+export const api = {
+    findAll: `http://${ip}:${port}/api/v1/user/findAll`,
+    findByName: `http://${ip}:${port}/api/v1/user/findByName`,
+    findById: `http://${ip}:${port}/api/v1/user/findById`,
+    insertOne: `http://${ip}:${port}/api/v1/user/insertOne`,
+    deletedById: `http://${ip}:${port}/api/v1/user/deletedById`,
+    deletedByName: `http://${ip}:${port}/api/v1/user/deletedByName`,
+    updateByName: `http://${ip}:${port}/api/v1/user/updateByName`,
+    updateById: `http://${ip}:${port}/api/v1/user/updateById`,
+};
