ca4536da950f9734f2fede39741f4d29cce86b01
kevin083177 <kevin083177@gmail.com>
2024-12-15 20:14:21 +0800

    fix api url

--- Frontend/src/enum/api.ts
+++ Frontend/src/enum/api.ts
@@ -1,13 +1,12 @@
 const ip = import.meta.env.VITE_IP;
-const port = import.meta.env.VITE_PORT;
 
 export const api = {
-    findAll: `http://${ip}:${port}/api/v1/user/findAll`,
-    findByName: `http://${ip}:${port}/api/v1/user/findByName`,
-    findById: `http://${ip}:${port}/api/v1/user/findById`,
-    insertOne: `http://${ip}:${port}/api/v1/user/insertOne`,
-    deletedById: `http://${ip}:${port}/api/v1/user/deletedById`,
-    deletedByName: `http://${ip}:${port}/api/v1/user/deletedByName`,
-    updateByName: `http://${ip}:${port}/api/v1/user/updateByName`,
-    updateById: `http://${ip}:${port}/api/v1/user/updateById`,
+    findAll: `http://${ip}/api/v1/user/findAll`,
+    findByName: `http://${ip}/api/v1/user/findByName`,
+    findById: `http://${ip}/api/v1/user/findById`,
+    insertOne: `http://${ip}/api/v1/user/insertOne`,
+    deletedById: `http://${ip}/api/v1/user/deletedById`,
+    deletedByName: `http://${ip}/api/v1/user/deletedByName`,
+    updateByName: `http://${ip}/api/v1/user/updateByName`,
+    updateById: `http://${ip}/api/v1/user/updateById`,
 };
